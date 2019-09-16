4f376589b5f278ea631d44fcc6d1e1bedfeacd56
leobancosta <leonardobancosta@gmail.com>
2019-09-17 03:58:26 +0800

    added mocha test report generator

--- test/employee-test.js
+++ test/employee-test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 var assert = require("assert");
 const path = require('path');
+const c = require('ansi-colors');
 
 describe('POST employee/authenticate', function() {
 	var server;
@@ -10,7 +11,7 @@ describe('POST employee/authenticate', function() {
 	afterEach(function () {
 		server.close();
 	});
-	it('responds with json', function(done) {
+	it('Positive Authentication with Status Code 200', function(done) {
 		request(server).post('/employee/authenticate')
 		.set('Content-Type','application/json')
 		.set('Authorization','Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1NjY4MjM1NzEsImV4cCI6MTU5ODM1OTU3MSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.i5tTEf7OwwKXiHi2yIXj3QGgLDHdnyzyLG1B9G1VzdI')
@@ -22,8 +23,8 @@ describe('POST employee/authenticate', function() {
 			done();
 		});
 	});
-	
-	it('responds with json', function(done) {
+
+	it('Negative Authentication with Status Code 400', function(done) {
 		request(server).post('/employee/authenticate')
 		.set('Content-Type','application/json')
 		.set('Authorization','Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1NjY4MjM1NzEsImV4cCI6MTU5ODM1OTU3MSwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.i5tTEf7OwwKXiHi2yIXj3QGgLDHdnyzyLG1B9G1VzdI')
